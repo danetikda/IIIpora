414ee8651c2a7f8eb0042b6b61e3ecb2f8f5508d
user <generalgafs540@gmail.com>
2025-03-02 20:28:40 +0300

    Pochti last

---
+++ 2_задание.py
@@ -0,0 +1,9 @@
+#Это файл со всеми типами решений задачь ЕГЭ по информатике
+# 2 задание Егэ по иноформатике
+print("x y z w")
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if not((x == (w or y)) or ((w <= z) and (y <= w))):
+                    print(x, y, z, w)
